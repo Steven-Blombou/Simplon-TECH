ba9babb5a900097d06c47488e1ef510b8e31177c
Steven-Blombou <blombouteven@gmail.com>
2020-03-18 09:27:33 +0100

    Commit controle romain

---
+++ contact.php
@@ -0,0 +1,154 @@
+<header>
+  <div class="container">
+    <div class="logo_uvp row">
+      <nav class="navbar navbar">
+        <a class="navbar-brand" href="#"><img src="img/site/logo_simplon_TECH.png" width="90" height="90" alt="logo_simplon_TECH.png"></a>
+        <span class="navbar-text">
+          Leader fr du renouvellement de matériel informatique
+        </span>
+      </nav>
+    </div>
+  </div>
+</header>
+
+
+<section class="navbar_collapse">
+  <div class="navbar">
+  <ul>
+    <li>
+      <a href="#">Accueil</a>
+    </li>
+    <li>
+      <a href="#">Contact</a>
+    </li>
+    <li>
+      <a href="#">Produits</a>
+    </li>
+  </ul>
+</div>
+</section>
+
+
+<section class="contact">
+  <div class="container">
+    <div class="cc-contact">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="card mb-0">
+            <div class="h4 text-center title">Contact Me"</div>
+            <div class="row">
+              <div class="col-md-9">
+                <div class="card-body">
+                  <form action="https://formspree.io/ " method="POST">
+                    <div class="p pb-3"><strong>Pour une config a toutes epreuves : Contatez nous:</strong></div>
+                    <div class="row mb-3">
+                      <div class="col">
+                        <div class="input-group"><span class="input-group-addon"><i class="fa fa-user-circle"></i></span>
+                          <input class="form-control" type="text" name="name" placeholder="Name" required="required"/>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row mb-3">
+                      <div class="col">
+                        <div class="input-group"><span class="input-group-addon"><i class="fa fa-file-text"></i></span>
+                          <input class="form-control" type="text" name="Subject" placeholder="Subject" required="required"/>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row mb-3">
+                      <div class="col">
+                        <div class="input-group"><span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                          <input class="form-control" type="email" name="_replyto" placeholder="E-mail" required="required"/>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row mb-3">
+                      <div class="col">
+                        <div class="form-group">
+                          <textarea class="form-control" name="message" placeholder="Your Message" required="required"></textarea>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col">
+                        <button class="btn btn-primary" type="submit">Send</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="banner">
+  <div class="container">
+    <h2>Welcome to the store</h2>
+  </div>
+</section>
+
+<section class="custom_iframe">
+  <div class="container">
+    <div class="row">
+      <div class="col-6 col-lg-12">
+        <iframe src="https://www.google.com/maps/d/embed?mid=18AVv66_CZqvhhdSmg-KwA0JI51ruVkOd" width="640" height="480"></iframe>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="testimonial">
+  <!--  -->
+</section>
+
+
+<section class="banner3">
+<!--  -->
+</section>
+
+
+<section class="banner_background">
+<!--  -->
+</section>
+
+
+<section class="social">
+  <div class="container">
+    <div class="row">
+      <div class="col-6 col-lg-4">
+        <iframe src="https://www.google.com/maps/d/embed?mid=18AVv66_CZqvhhdSmg-KwA0JI51ruVkOd" width="320" height="240"></iframe>
+      </div>
+  <div class="col-6 col-lg-4">
+    <div class="social-networks text-lg-left">
+      <a href="https://fr-fr.facebook.com/"><img src="img/site/facebook.svg" alt="facebook.svg"></a><!-- enleve l espace qui existe par defaut -->
+      <a href="https://www.instagram.com/?hl=fr"><img src="img/site/instagram.svg" alt="instagram.svg"></a>
+      <a href="https://twitter.com/?lang=fr"><img src="img/site/twitter.svg" alt="twitter.svg"></a>
+    </div>
+  </div>
+  <div class="col-6 col-lg-4">
+    <p><span>Adresse :</span> Parc de haute technologie du Moulin Leblanc, Campus Sup, 08000 Charleville-Mézières<br>
+    <span>Telephnone : </span> +33 3 24 56 62 66<br>
+   </p>
+  </div>
+  </div>
+</div>
+</section>
+
+
+<footer>
+  <div class="container">
+      <div class="row">
+        <div class="col-6 col-lg -12">
+         <div class="copyright">
+          <p>Copyright SIMPLON CHARLEVILLE - All Right Reserved</p>
+         </div>
+        </div>
+      </div>
+  </div>
+</footer>
